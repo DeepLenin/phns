c5cd24f3bab152986efc6c407c83c0aaf05cef3b
Alex Gaziev <alex.gaziev@gmail.com>
2020-04-07 11:00:20 -0700

    Add timit bench data to repo

--- bench/timit.py
+++ bench/timit.py
@@ -1,39 +1,34 @@
-import academictorrents as at
 import numpy as np
 import os
-from glob import glob
+from tqdm import tqdm
+import pickle
 
 import sys
 sys.path.append('../phns')
 import phns
 
-root = os.path.expanduser("~/.academictorrents-datastore/TIMIT")
 
-if not os.path.exists(root):
-    from zipfile import ZipFile
-    with ZipFile(at.get("34e2b78745138186976cbc27939b1b34d18bd5b3")) as zipped:
-        zipped.extractall(root)
+DATA_PATH = os.path.dirname(os.path.realpath(__file__)) + "/../data/"
 
-data = []
-for text_path in glob(root + "/data/lisa/data/timit/raw/TIMIT/*/*/*/*.TXT"):
-    phns_path = text_path.replace(".TXT", ".PHN")
-
-    data.append(dict(
-        text = open(text_path).read().split(" ", 2)[-1],
-        phns = [phn.split(" ")[-1] for phn in open(phns_path).read().split("\n") if phn]
-    ))
+print(f"Loading data from {DATA_PATH}")
+with open(DATA_PATH + "timit_bench.pkl", "rb") as f:
+    data = pickle.load(f)
 
 
 cers = []
-for item in data:
+for item in tqdm(data):
+    # Preprocessing data
+    _phns = phns.utils.remap(item["phns"])
+    _phns = [phn for phn in _phns if phn != "sil"]
+
     calculated_phns_variants = phns.from_text(item["text"])
     if not calculated_phns_variants:
         continue
 
-    best = phns.closest(item["phns"], calculated_phns_variants)
+    best = phns.closest(_phns, calculated_phns_variants)
     cers.append(best["cer"])
 
-print(cers)
+# print(cers)
 print({
     '25%': np.percentile(cers, 25),
     '50%': np.percentile(cers, 50),
