9115b61961f6df7e772625f80e584ffbb7d7bb9e
brainopia <brainopia@evilmartians.com>
2020-04-24 19:50:34 +0300

    Initial template for graph

--- phns/text.py
+++ phns/text.py
@@ -2,6 +2,7 @@ import os
 import itertools
 from copy import deepcopy
 from . import heuristics
+from .graph import Graph
 from .utils import cmu, deep_phn
 
 
@@ -26,19 +27,17 @@ def from_text(text, missing_handler=lambda _: False, apply_heuristics=True):
     if not callable(missing_handler):
         raise TypeError("missing_handler should be callable")
 
-    cmu_phns = []
     skip = False
+    graph = Graph()
 
     for word in words:
         transcription = deepcopy(cmu.get(word)) or deep_phn(missing_handler(word))
         if transcription:
-            cmu_phns.append(transcription)
+            graph.attach(transcription)
         else:
             skip = True
 
     if not skip:
-        different_pronunciations = list(itertools.product(*cmu_phns))
-        different_pronunciations = [list(pron) for pron in different_pronunciations]
         if apply_heuristics:
-            different_pronunciations = heuristics.apply(different_pronunciations)
-        return different_pronunciations
+            graph = heuristics.apply(graph)
+        return graph
