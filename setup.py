3451c7fcfbe2173d09bdb3d8b86bf404b418430e
brainopia <brainopia@evilmartians.com>
2020-06-04 20:53:40 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="phns",
+    version="0.0.1",
+    author="Deep Lenin",
+    description="A small package",
+    packages=["numpy", "scipy"],
+    python_requires=">=3.6",
+)
